5cf5a5480c887ecdf4fa017e6adc808103102c4c
Arnold Omondi <arnoldomondi7@gmail.com>
2023-01-11 00:13:51 +0300

    working on the about image

--- frontend/src/components/AboutUs.comp.jsx
+++ frontend/src/components/AboutUs.comp.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
-import { FaQuoteLeft } from 'react-icons/fa'
+import {
+	FaFacebook,
+	FaInstagram,
+	FaLinkedin,
+	FaQuoteLeft,
+	FaTwitter,
+	FaYoutube,
+} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const AboutUs = () => {
 	return (
@@ -41,7 +49,158 @@ const AboutUs = () => {
 								magni.
 							</span>
 						</div>
-						<div className='about-us-bottom-about-us'></div>
+						<div className='about-us-bottom-socials'>
+							<Link to='/'>
+								<FaFacebook />
+							</Link>
+							<Link to='/'>
+								<FaInstagram />
+							</Link>
+							<Link to='/'>
+								<FaTwitter />
+							</Link>
+							<Link to='/'>
+								<FaYoutube />
+							</Link>
+							<Link to='/'>
+								<FaLinkedin />
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className='about-us-others'>
+				<div className='about-us-content'>
+					<h2>Meet The Brilliant Team</h2>
+					<span>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</span>
+				</div>
+
+				<div className='about-us-images'>
+					<div className='about-us-image'>
+						<div className='about-us-image-top'>
+							<img src='./assets/uploads/seller-1.jpg' alt='' />
+							<div className='about-us-image-hover'>
+								<Link to='/'>
+									<FaFacebook />
+								</Link>
+								<Link to='/'>
+									<FaInstagram />
+								</Link>
+								<Link to='/'>
+									<FaTwitter />
+								</Link>
+								<Link to='/'>
+									<FaYoutube />
+								</Link>
+								<Link to='/'>
+									<FaLinkedin />
+								</Link>
+							</div>
+						</div>
+
+						<div className='about-us-image-bottom'>
+							<h4>Sales Team </h4>
+							<span>Alpha Sales Strategy</span>
+						</div>
+					</div>
+					<div className='about-us-image'>
+						<div className='about-us-image-top'>
+							<img src='./assets/uploads/seller-2.jpg' alt='' />
+							<div className='about-us-image-hover'>
+								<Link to='/'>
+									<FaFacebook />
+								</Link>
+								<Link to='/'>
+									<FaInstagram />
+								</Link>
+								<Link to='/'>
+									<FaTwitter />
+								</Link>
+								<Link to='/'>
+									<FaYoutube />
+								</Link>
+								<Link to='/'>
+									<FaLinkedin />
+								</Link>
+							</div>
+						</div>
+
+						<div className='about-us-image-bottom'>
+							<h4>John Doe</h4>
+							<span>Marketing Secretary</span>
+						</div>
+					</div>
+					<div className='about-us-image'>
+						<div className='about-us-image-top'>
+							<img src='./assets/uploads/seller-3.jpg' alt='' />
+							<div className='about-us-image-hover'>
+								<Link to='/'>
+									<FaFacebook />
+								</Link>
+								<Link to='/'>
+									<FaInstagram />
+								</Link>
+								<Link to='/'>
+									<FaTwitter />
+								</Link>
+								<Link to='/'>
+									<FaYoutube />
+								</Link>
+								<Link to='/'>
+									<FaLinkedin />
+								</Link>
+							</div>
+						</div>
+
+						<div className='about-us-image-bottom'>
+							<h4>Good Vybes</h4>
+							<span>Check Out Secretary</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className='about-us-others'>
+				<div className='about-us-content'>
+					<h2>Our Services </h2>
+					<span>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</span>
+				</div>
+
+				<div className='about-us-images-div'>
+					<div className='about-us-image-div'>
+						<h4>Jane Doe</h4>
+						<span>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+							laborum vero exercitationem iure, nulla aut quaerat impedit,
+							officia quia iste facere fugit, dicta excepturi voluptate harum
+							delectus.
+						</span>
+
+						<img src='./assets/uploads/jane-doe.jpg' alt='' />
+					</div>
+
+					<div className='about-us-image-div'>
+						<h4>Jane Doe</h4>
+						<span>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+							laborum vero exercitationem iure, nulla aut quaerat impedit,
+							officia quia iste facere fugit, dicta excepturi voluptate harum
+							delectus.
+						</span>
+
+						<img src='./assets/uploads/jane-doe.jpg' alt='' />
+					</div>
+
+					<div className='about-us-image-div'>
+						<h4>Jane Doe</h4>
+						<span>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+							laborum vero exercitationem iure, nulla aut quaerat impedit,
+							officia quia iste facere fugit, dicta excepturi voluptate harum
+							delectus.
+						</span>
+
+						<img src='./assets/uploads/jane-doe.jpg' alt='' />
 					</div>
 				</div>
 			</div>
